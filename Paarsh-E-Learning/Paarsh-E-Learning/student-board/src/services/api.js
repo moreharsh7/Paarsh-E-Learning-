b88fcb1b301a80f2e591cd44f9803fdacceb5c1c
Harshad More <moreharshad7070@gmail.com>
2026-04-01 13:02:58 +0530

    i have deployed the backend on the render and frontnend on the vercel so the new url of the backend is commited here
    i have deployed the backend on the render and frontnend on the vercel so the new url of the backend is commited here

--- Paarsh-E-Learning/Paarsh-E-Learning/student-board/src/services/api.js
+++ Paarsh-E-Learning/Paarsh-E-Learning/student-board/src/services/api.js
@@ -1,6 +1,6 @@
 // src/services/api.js
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://your-backend-name.onrender.com/api';
 
 export const getCourses = async (filters = {}) => {
   try {
