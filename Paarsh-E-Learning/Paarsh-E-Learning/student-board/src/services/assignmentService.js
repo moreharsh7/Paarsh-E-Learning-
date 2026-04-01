d58104ad5b7889d05936283e180deaefde605bb6
Harshad More <moreharshad7070@gmail.com>
2026-04-01 13:18:50 +0530

    Update assignmentService.js

--- Paarsh-E-Learning/Paarsh-E-Learning/student-board/src/services/assignmentService.js
+++ Paarsh-E-Learning/Paarsh-E-Learning/student-board/src/services/assignmentService.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 =================================================== */
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://paarsh-e-learning-p.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
