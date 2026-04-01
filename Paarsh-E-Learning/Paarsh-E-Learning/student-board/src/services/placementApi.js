cce0f36d33980ca840d95f98c755c485a78d6c5c
Harshad More <moreharshad7070@gmail.com>
2026-04-01 13:21:29 +0530

    Update placementApi.js

--- Paarsh-E-Learning/Paarsh-E-Learning/student-board/src/services/placementApi.js
+++ Paarsh-E-Learning/Paarsh-E-Learning/student-board/src/services/placementApi.js
@@ -1,5 +1,5 @@
 // In your frontend placementApi.js
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://paarsh-e-learning-p.onrender.com/api';
 
 // Get placement data for a student and course
 export const getPlacementData = async (studentId, courseId) => {
